bce55fc63e42b3737e43e700ae92b98d01df91f7
Евгений <pomponchik@MacBook-Pro-Evgenij.local>
2023-04-09 21:09:23 +0300

    tests

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,3 +1,7 @@
+import importlib
+
+import pytest
+
 import installed
 
 
@@ -10,3 +14,19 @@ def test_polog_install_two_and_import():
     with installed('polog', 'astrologic'):
         from polog import log, config, file_writer
         from astrologic import switcher
+
+
+def test_polog_install_two_contexts_and_import():
+    with installed('polog'):
+        with installed('astrologic'):
+            from polog import log, config, file_writer
+            from astrologic import switcher
+
+
+def test_deleting_contexts():
+    with installed('polog'):
+        with installed('astrologic'):
+            pass
+    with pytest.raises(ModuleNotFoundError):
+        import polog
+        importlib.reload(polog)

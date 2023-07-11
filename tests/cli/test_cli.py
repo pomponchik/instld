3def39b1d25bebc1000029a403d5cb24858175c2
Evgeniy Blinov <zheni-b@yandex.ru>
2023-07-11 17:39:03 +0300

    a new test

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -1,1 +1,10 @@
+import os
+import subprocess
+
+
 def test_basic_cli():
+    result = subprocess.run(['instld', os.path.join('tests', 'cli', 'quasi_empty_main.py')], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    print(result.stdout.decode('utf-8'))
+    print(result.stderr.decode('utf-8'))
+    result.check_returncode()

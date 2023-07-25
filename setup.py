00992406050e72d896ff9a1e92c983ff85d28815
Evgeniy Blinov <zheni-b@yandex.ru>
2023-07-25 16:16:16 +0300

    fix readme reading in the setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-with open("README.md", "r") as readme_file:
+with open("README.md", "r", encoding="utf8") as readme_file:
     readme = readme_file.read()
 
 requirements = []

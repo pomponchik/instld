41bb6b36b4f66cf928db1c090f316d4be4fe74ba
Evgeniy Blinov <zheni-b@yandex.ru>
2023-07-25 17:54:32 +0300

    prints

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -5,8 +5,8 @@ import shutil
 
 def test_cli_where():
     strings = [
-        f'import is_odd  # instld: where {os.path.join("tests", "cli", "data", "pok")}',
-        f'import is_even  # instld: where {os.path.join("tests", "cli", "data", "chpok")}',
+        rf'import is_odd  # instld: where {os.path.join("tests", "cli", "data", "pok")}',
+        rf'import is_even  # instld: where {os.path.join("tests", "cli", "data", "chpok")}',
         'assert is_odd.valid(23)',
         'assert is_even.isEven(1)',
     ]

0f4e90f98f13dcbffc340935478e22c61ee3e2cf
Evgeniy Blinov <zheni-b@yandex.ru>
2023-07-28 16:02:38 +0300

    test

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -4,15 +4,29 @@ import subprocess
 import shutil
 
 import pytest
+from termcolor import colored
 
+
+@pytest.hookimpl
+def pytest_runtest_makereport(item, call):
+    """
+    Хук, добавляющий информацию о текущих настройках к каждому выводу об ошибке в тесте.
+    """
+    if call.when == 'call':
+        if call.excinfo:
+            item.add_report_section("call", "config", colored(open(os.path.join("tests", "cli", "data", "test.log"), 'r').read(), 'cyan'))
 
 @pytest.mark.timeout(60)
 def test_cli_where():
     strings = [
         rf'import is_odd  # instld: where {os.path.join("tests", "cli", "data", "pok")}',
+        'open("%s", "a").write("step 1")' % (os.path.join("tests", "cli", "data", "test.log"), ),
         rf'import is_even  # instld: where {os.path.join("tests", "cli", "data", "chpok")}',
+        'open("%s", "a").write("step 2")' % (os.path.join("tests", "cli", "data", "test.log"), ),
         'assert is_odd.valid(23)',
+        'open("%s", "a").write("step 3")' % (os.path.join("tests", "cli", "data", "test.log"), ),
         'assert is_even.isEven(1)',
+        'open("%s", "a").write("step 4")' % (os.path.join("tests", "cli", "data", "test.log"), ),
     ]
 
     script = os.path.join('tests', 'cli', 'data', 'main.py')

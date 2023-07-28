70f23cede21d9752289c5a7579ac4336b73492f1
Evgeniy Blinov <zheni-b@yandex.ru>
2023-07-28 13:15:17 +0300

    without prints

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -18,10 +18,6 @@ def test_cli_where():
 
     result = subprocess.run(['instld', script], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    print(script)
-    print(strings)
-    print(result.stdout.decode('utf-8'))
-    print(result.stderr.decode('utf-8'))
     result.check_returncode()
 
     base_libs_paths = {
@@ -31,20 +27,9 @@ def test_cli_where():
 
     for path, library_name in base_libs_paths.items():
         full_path_to_the_lib = os.path.join(path, 'lib')
-        print(sys.platform, sys.platform.lower())
         if sys.platform.lower() not in ('win32',):
-            print('NOT WIN!')
             full_path_to_the_lib = os.path.join(full_path_to_the_lib, os.path.basename(os.listdir(path=full_path_to_the_lib)[0]), 'site-packages')
-        else:
-            print('ELSE')
-            print('listdir 0:', os.listdir(path=full_path_to_the_lib))
-            #full_path_to_the_lib = os.path.join(full_path_to_the_lib, 'site-packages')
-        print('path!:', full_path_to_the_lib)
-        print('listdir 1:', os.listdir(path=full_path_to_the_lib))
         full_path_to_the_lib = os.path.join(full_path_to_the_lib, library_name)
-
-        print('full_path_to_the_lib:', full_path_to_the_lib)
-
 
         assert os.path.isdir(full_path_to_the_lib)
 

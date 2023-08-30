67409ee3950fa15b9eac37ccff59cae911629de8
Evgeniy Blinov <zheni-b@yandex.ru>
2023-08-30 18:52:41 +0300

    fix of a fixture

--- installed/cli/parsing_arguments/get_python_file.py
+++ installed/cli/parsing_arguments/get_python_file.py
@@ -1,9 +1,10 @@
+import os
 import sys
 
 
 def get_python_file():
     if len(sys.argv) < 2:
-        print('usage: instld python_file.py [argv ...]', file=sys.stderr)
+        print('usage: instld python_file.py [argv ...]', file=sys.stderr, end=os.linesep)
         sys.exit(1)
 
     return sys.argv[1]

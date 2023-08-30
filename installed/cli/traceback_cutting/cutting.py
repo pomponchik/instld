cfe67c49e3d3834d7aea6e28cbbc763b157f633b
Evgeniy Blinov <zheni-b@yandex.ru>
2023-08-30 14:59:38 +0300

    linesep awared execthook

--- installed/cli/traceback_cutting/cutting.py
+++ installed/cli/traceback_cutting/cutting.py
@@ -15,7 +15,7 @@ def create_cutting_excepthook(old_hook, base_size):
         with redirect_stderr(buffer):
             traceback.print_exception(exc_type, value, traceback_object)
         traceback_string = buffer.getvalue()
-        if sys.platform.lower() not in ('win32',):
+        if sys.platform.lower() in ('win32',):
             traceback_string = traceback_string.replace('\n', os.linesep)
         print(traceback_string, file=sys.stderr, end='')
     return new_hook

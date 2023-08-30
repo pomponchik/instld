92ef84227b117a71ec261d5b4d90eda9a3679530
Evgeniy Blinov <zheni-b@yandex.ru>
2023-08-31 02:41:04 +0300

    pseudo test

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -109,3 +109,8 @@ def test_exceptions_are_similar_with_just_python_command(main_runner):
             assert result_1.stderr == result_2.stderr
 
             os.remove(script)
+
+
+def test_test():
+    if sys.platform.lower() in ('win32',):
+        print('linesep:', os.linesep)

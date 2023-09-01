4154e8542649b90b52b7dbe4267c0c362b347a27
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-01 18:31:38 +0300

    fix output

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -58,6 +58,11 @@ def test_run_command_without_arguments(main_runner):
         assert result.stderr.decode('utf-8') == f'usage: instld python_file.py [argv ...]{os.linesep}'
 
 
+def test_test():
+    print(b'usage: instld python_file.py [argv ...]'.decode('utf-8') == f'usage: instld python_file.py [argv ...]{os.linesep}')
+    assert b'usage: instld python_file.py [argv ...]'.decode('utf-8') == f'usage: instld python_file.py [argv ...]{os.linesep}'
+
+
 def test_run_command_with_arguments(main_runner):
     strings = [
         'import json, sys',

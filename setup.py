1929883a9f43f6627033f8c4d34505292fb7382d
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-06 02:05:51 +0300

    new version tag

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requirements = []
 
 setup(
     name='instld',
-    version='0.0.20',
+    version='0.0.21',
     author='Evgeniy Blinov',
     author_email='zheni-b@yandex.ru',
     description='The simplest package management',

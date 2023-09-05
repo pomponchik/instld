cb53e8a13c22421b07414f74afef832d0808aab4
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-06 01:24:56 +0300

    delete extra comments

--- installed/cli/traceback_cutting/cutting.py
+++ installed/cli/traceback_cutting/cutting.py
@@ -10,17 +10,6 @@ from installed.cli.traceback_cutting.traceback_utils import cut_base_of_tracebac
 def create_cutting_excepthook(old_hook, base_size):
     def new_hook(exc_type, value, traceback_object):
         traceback_object = cut_base_of_traceback(traceback_object, base_size)
-
-        #buffer = io.StringIO()
-        #with redirect_stderr(buffer):
-        #    traceback.print_exception(exc_type, value, traceback_object)
-        #traceback_string = buffer.getvalue()
-        #if sys.platform.lower() in ('win32',):
-        #    traceback_string = traceback_string.replace(os.linesep, '\n')
-        #    traceback_string = traceback_string.replace('\n', os.linesep)
-
-
-        #print(traceback_string, file=sys.stderr, end='')
         traceback.print_exception(exc_type, value, traceback_object)
     return new_hook
 

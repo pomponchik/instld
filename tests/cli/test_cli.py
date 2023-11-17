d45599e5d1f16e98f205bcbe777329631b2fd58c
Evgeniy Blinov <zheni-b@yandex.ru>
2023-11-18 00:16:49 +0300

    prints

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -113,6 +113,8 @@ def test_exceptions_are_similar_with_just_python_command_2():
 
         assert result_1.returncode == result_2.returncode
         assert result_1.stdout == result_2.stdout
+        print('1:', result_1.stderr)
+        print('2:', result_2.stderr)
         assert result_1.stderr == result_2.stderr
 
         os.remove(script)

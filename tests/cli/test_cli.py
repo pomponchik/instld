c3fb449145927db8444a5cdd1abc7f74d881d3ab
Evgeniy Blinov <zheni-b@yandex.ru>
2023-11-20 23:01:06 +0300

    fixed tests

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -13,7 +13,7 @@ def test_cli_where(main_runner):
         rf'import is_odd  # instld: where {os.path.join("tests", "cli", "data", "pok")}',
         rf'import is_even  # instld: where {os.path.join("tests", "cli", "data", "chpok")}',
         'assert is_odd.valid(23)',
-        'assert is_even.isEven(1)',
+        'assert is_even.is_even(2)',
     ]
 
     script = os.path.join('tests', 'cli', 'data', 'main.py')

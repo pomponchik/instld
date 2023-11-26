9204e3f78954bd1d417e15f19fe8d852a7bd3222
Evgeniy Blinov <zheni-b@yandex.ru>
2023-11-26 23:14:15 +0300

    cutting trace size

--- instld/cli/traceback_cutting/traceback_utils.py
+++ instld/cli/traceback_cutting/traceback_utils.py
@@ -9,8 +9,15 @@ def count_traceback_lenth(traceback_object):
 def cut_base_of_traceback(traceback_object, base_size):
     index = 0
     while traceback_object is not None:
-        #print(traceback_object.tb_frame.f_code.co_qualname, dir(traceback_object.tb_frame.f_code.co_qualname))
+        #print(traceback_object.tb_frame.f_code.co_qualname, traceback_object.tb_frame.f_code.co_filename)
         if index == base_size:
             return traceback_object
         index += 1
+        traceback_object = traceback_object.tb_next
+
+
+def cut_importlib_bug(traceback_object):
+    while traceback_object is not None:
+        if not (traceback_object.tb_frame.f_code.co_qualname == '_call_with_frames_removed' and traceback_object.tb_frame.f_code.co_filename == '<frozen importlib._bootstrap>'):
+            return traceback_object
         traceback_object = traceback_object.tb_next

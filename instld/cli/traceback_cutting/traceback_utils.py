162976c7e1ee6c49d3f6f8af41d0a0c57ed85659
Evgeniy Blinov <zheni-b@yandex.ru>
2023-11-28 13:20:38 +0300

    no prints

--- instld/cli/traceback_cutting/traceback_utils.py
+++ instld/cli/traceback_cutting/traceback_utils.py
@@ -9,7 +9,6 @@ def count_traceback_lenth(traceback_object):
 def cut_base_of_traceback(traceback_object, base_size):
     index = 0
     while traceback_object is not None:
-        #print(traceback_object.tb_frame.f_code.co_qualname, traceback_object.tb_frame.f_code.co_filename)
         if index == base_size:
             return traceback_object
         index += 1
